0026d92ded1b962500173b706971095fbe114347
SHEZ <shezcode@gmail.com>
2023-05-29 11:20:34 +0200

    auth pages

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt'
 import {SignJWT, jwtVerify} from 'jose'
+import {db} from './db'
 
 export const hashPassword = (password) => bcrypt.hash(password, 10)
 
@@ -9,7 +10,7 @@ export const comparePasswords = (plainTextPassword, hashedPassword) => {
 
 export const createJWT = user => {
   const iat = Math.floor(Date.now() / 1000)
-  const exp = iat = 60 * 60 * 24 * 7
+  const exp = iat + 60 * 60 * 24 * 7
 
   return new SignJWT({ payload: { id: user.id, email: user.email }})
     .setProtectedHeader({ alg: "HS256", typ: "JWT"})
@@ -26,4 +27,18 @@ export const validateJWT = async (jwt) => {
   )
 
   return payload.payload as any;
+}
+
+export const getUserFromCookie = async (cookies) => {
+  const jwt = cookies.get(process.env.COOKIE_NAME)
+
+  const {id} = await validateJWT(jwt)
+
+  const user = await db.user.findUnique({
+    where: {
+      id
+    }
+  })
+
+  return user
 }
