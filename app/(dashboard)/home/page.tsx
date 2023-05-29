0ac70052e5ef0cea3aa89c3e448bc98142ddc0e3
SHEZ <shezcode@gmail.com>
2023-05-29 23:21:13 +0200

    skeleton

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,4 +1,5 @@
 import Greetings from "@/components/Greetings";
+import GreetingSkeleton from "@/components/GreetingSkeleton";
 import { delay } from "@/lib/async";
 import { getUserFromCookie } from "@/lib/auth";
 import { db } from "@/lib/db";
@@ -8,10 +9,12 @@ import { Suspense } from "react";
 
 const Home = () => {
   return (
-    <div className="h-full overflow-y-auto pr-6 w-1/1">
+    <div className="h-full overflow-y-auto pr-6 w-full">
       <div className="h-full items-stretch justify-center min-h-[content]">
         <div className="flex-1 grow flex">
-          <Greetings />
+          <Suspense fallback={<GreetingSkeleton />}>
+            <Greetings />
+          </Suspense>
         </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3">
           {/** projects map here*/}
--- components/GreetingSkeleton.tsx
+++ components/GreetingSkeleton.tsx
@@ -8,10 +8,16 @@ const GreetingSkeleton = () => {
         <div className="flex-1 space-y-6 py-1">
           <div className="h-2 bg-gray-300 rounded"></div>
           <div className="space-y-3">
-            <div className="grid grid-cols-3 gap-4"></div>
+            <div className="grid grid-cols-3 gap-4">
+              <div className="h-2 bg-gray-300 rounded col-span-2"></div>
+              <div className="h-2 bg-gray-300 rounded col-span-1"></div>
+            </div>
+            <div className="h-2 bg-gray-300 rounded"></div>
           </div>
         </div>
       </div>
     </Card>
   )
 }
+
+export default GreetingSkeleton
